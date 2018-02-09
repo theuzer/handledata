ba92b535a00abfc94715a72ee26f83729484a0b3
theuzer <jarvandemacio@gmail.com>
2018-02-09 00:21:09 +0000

    devs

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
 });
 
 ontime({
-  cycle: ['01:00:00'],
+  cycle: ['00:25:00'],
 }, (ot) => {
   getTelemetries.executeQuery(getTelemetries.getYesterdayStatsQueryByDate(null));
   ot.done();
